a4eda94e38bd9983ef3e0f59df8d6a5c47481506
Jan Staniszewski <jan.staniszewski@bmw.partner.de>
2026-07-02 23:51:36 +0200

    feat: add TreeItem subclasses for org groups, orgs, details and actions

---
+++ src/tree/treeItems.ts
@@ -0,0 +1,38 @@
+// src/tree/treeItems.ts
+import * as vscode from 'vscode';
+import { OrgSummary, ConnectionStatus } from '../models/org';
+
+export class OrgGroupItem extends vscode.TreeItem {
+  constructor(public readonly groupName: string, public readonly orgs: OrgSummary[]) {
+    super(groupName, vscode.TreeItemCollapsibleState.Expanded);
+    this.contextValue = 'orgGroup';
+  }
+}
+
+export class OrgItem extends vscode.TreeItem {
+  constructor(public readonly org: OrgSummary) {
+    super(org.alias ?? org.username, vscode.TreeItemCollapsibleState.Collapsed);
+    this.description = `${org.username}${org.isDefault ? ' (domyślna)' : ''}`;
+    this.contextValue = 'org';
+    this.iconPath = new vscode.ThemeIcon(
+      org.status === ConnectionStatus.Connected ? 'circle-filled' : 'warning',
+      new vscode.ThemeColor(org.status === ConnectionStatus.Connected ? 'charts.green' : 'charts.red')
+    );
+  }
+}
+
+export class OrgDetailItem extends vscode.TreeItem {
+  constructor(label: string, value: string) {
+    super(`${label}: ${value}`, vscode.TreeItemCollapsibleState.None);
+    this.contextValue = 'orgDetail';
+  }
+}
+
+export class OrgActionItem extends vscode.TreeItem {
+  constructor(label: string, icon: string, commandId: string, org: OrgSummary) {
+    super(label, vscode.TreeItemCollapsibleState.None);
+    this.iconPath = new vscode.ThemeIcon(icon);
+    this.contextValue = 'orgAction';
+    this.command = { command: commandId, title: label, arguments: [org] };
+  }
+}
